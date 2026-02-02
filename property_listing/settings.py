7f27e0cf7d2f668f63b1f8931ce374f2d986f78b
Asseton1 <asseton@asseton.in>
2026-02-02 13:44:59 +0530

    Update settings.py

--- property_listing/settings.py
+++ property_listing/settings.py
@@ -40,6 +40,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.asseton.in",
     "https://asseton.in",
 ]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
 # Application definition
