4caf015d252a9814ef3f375130db9224998a2f11
Asseton1 <asseton@asseton.in>
2026-07-08 15:36:27 +0530

    Update apps.py

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Register cache-invalidation signals for locations endpoint.
+        from . import signals  # noqa: F401
